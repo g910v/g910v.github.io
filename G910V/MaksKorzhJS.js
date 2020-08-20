9271993d7be1f388d12c15d2343b2272e1c5541e
g910v <69211469+g910v@users.noreply.github.com>
2020-08-20 18:39:12 +0300

    Add files via upload

---
+++ G910V/MaksKorzhJS.js
@@ -0,0 +1,18 @@
+new Vue ({
+	el: '#ForMenu',
+	data: {
+		show: false
+	}
+})
+
+barba.init({
+    transitions: [{
+        name: 'opacity-transition',
+        leave() {
+            
+        },
+        enter() {
+            
+        }
+    }]
+});
