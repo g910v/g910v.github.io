371382040033a61fc090c6debf9067b6a693632f
g910v <69211469+g910v@users.noreply.github.com>
2020-08-24 21:06:49 +0300

    Add files via upload

---
+++ G910VJS.js
@@ -0,0 +1,16 @@
+new Vue ({
+	el: '#ForMenu',
+	data: {
+		show: false
+	}
+})
+
+new Vue ({
+	el: '.MainSection',
+	data: {
+		CurrentAlbom: 1
+	}
+})
+
+// import Swup from 'swup'
+const swup = new Swup();
